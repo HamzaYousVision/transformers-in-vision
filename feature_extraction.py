d4c22842245a6352a9c46417a51f47477b78c08e
Hamza Yous <hyous@usthb.dz>
2023-03-21 06:55:13 +0000

    feat: add features visualization

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,25 +1,61 @@
+import os
+import random
+from PIL import Image
+from matplotlib import pyplot as plt
 import torch
 import torchextractor as tx
+from torchvision import transforms
 
-class FeatureExtractor: 
+
+class FeatureExtractor:
     def __init__(self, model):
+        self.image_input = None
+        self.input_size = (224, 224)
         self.model = model
 
-    def extract_features(self, image_input=None): 
+    def set_input_image(self, random_input=False):
+        if random_input:
+            self.image_input = torch.rand(
+                torch.Size((1, 3, self.input_size[0], self.input_size[1]))
+            )
+        else:
+            self.image_input = Image.open(os.path.join("data", "sample_input.jpg"))
+            self.image_input = self.generate_input(self.image_input).unsqueeze(0)
+
+    def extract_features(self):
+        assert self.image_input is not None, "please reset image first"
         layers_names = [name for name, _ in self.model.named_modules()]
         model_tx = tx.Extractor(self.model, layers_names)
+        _, self.features = model_tx(self.image_input)
 
-        if image_input is None: 
-            input_activation_size = torch.Size((1, 3, 224, 224)) 
-            image_input = torch.rand(input_activation_size)
+    def generate_input(self, input):
+        transform = transforms.Compose(
+            [
+                transforms.Resize(self.input_size),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406],
+                    std=[0.229, 0.224, 0.225],
+                ),
+            ]
+        )
+        return transform(input)
 
-        _, features = model_tx(image_input)
-        activation_shapes = {name: f for name, f in features.items()}
-
-        activation_shapes = {}
-        for name, f in features.items():
+    def show_info(self):
+        for name, f in self.features.items():
             if type(f) == tuple:
-                activation_shapes[name] = f[0].shape
-            else: 
-                activation_shapes[name] = f.shape 
+                print(name, f[0].shape, sep="  ")
+            else:
+                print(name, f.shape, sep="  ")
 
+    def visualize_features(self, features_index):
+        slected_layer = list(self.features)[features_index]
+        selected_feature = self.features[slected_layer]
+
+        channel_indexes = random.sample(range(selected_feature.shape[-1]), 50)
+        for channel_index in channel_indexes:
+            plan = selected_feature[0, :, :, channel_index].cpu().detach().numpy()
+
+            plt.imshow(plan)
+            plt.pause(0.1)
+            plt.draw
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
 import argparse
+from matplotlib import pyplot as plt
 
 from torchvision.models import vit_b_16, resnet50, swin_b
 from feature_extraction import FeatureExtractor
@@ -8,13 +8,13 @@ from attention_visualization import AttentionVisualization
 
 
 def main(args):
-    if args.model == "vit": 
+    if args.model == "vit":
         model = vit_b_16()
     elif args.model == "resnet":
         model = resnet50()
     elif args.model == "swin":
-        model = swin_b() 
-    else: 
+        model = swin_b()
+    else:
         SystemExit("Not Valid")
 
     if args.structure:
@@ -24,16 +24,18 @@ def main(args):
         attention_visualization.visualize()
     if args.features:
         features_extractor = FeatureExtractor(model)
+        features_extractor.set_input_image()
         features_extractor.extract_features()
+        features_extractor.show_info()
+        features_extractor.visualize_features(10)
 
 
 if __name__ == "__main__":
- 
     parser = argparse.ArgumentParser(description="Model Structure Understanding Script")
     parser.add_argument("--model", type=str, help="model name")
     parser.add_argument("--features", action="store_true", help="feature extraction")
     parser.add_argument("--attention", action="store_true", help="visualize attention")
     parser.add_argument("--structure", action="store_true", help="show model structure")
     args = parser.parse_args()
-    
+
     main(args)
